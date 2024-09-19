be25aec62db3260a440775d31358c6e82eb79e2f
Emmett Omron <118745629+emomron2025@users.noreply.github.com>
2024-09-19 07:59:57 -0400

    Update getscore.ps1

--- getscore.ps1
+++ getscore.ps1
@@ -5,24 +5,19 @@ $ScoringDir = "\." #put the absolute file path of your scoring directory between
 # if you intend to break internet access in your image, add a local copy of scorebot.ps1 in your scoring directory
 
 
-
-
 try {
   $Response = Invoke-WebRequest -Uri $ScorebotLink -ErrorAction Stop
   $Response.Content | Out-File -FilePath "$ScoringDir\scorebot.ps1"  
 }
 catch {
   Write-Output "Failed to download scorebot.ps1. Attempting to access local copy..."
-
 }
 
 try {
   & $ScoringDir\scorebot.ps1
-}
-catch {
+} catch {
   Write-Error "Failed to execute scorebot.ps1"
-}
-finally {
+} finally {
   if (Test-Path -Path "$ScoringDir\scorebot.ps1") {
     Remove-Item -Path "$ScoringDir\scorebot.ps1" -ErrorAction SilentlyContinue
   }
